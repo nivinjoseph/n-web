2f142af09980691f22a9d855cb242385c407eb9d
nivinjoseph <nivinjoseph@outlook.com>
2021-04-30 22:14:27 -0400

    package updates and dist clenaup

--- dist/exceptions/http-redirect-exception.d.ts
+++
@@ -1,7 +0,0 @@
-import { Exception } from "@nivinjoseph/n-exception";
-import "@nivinjoseph/n-ext";
-export declare class HttpRedirectException extends Exception {
-    private readonly _url;
-    get url(): string;
-    constructor(url: string);
-}
