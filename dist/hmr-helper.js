cbcc2038578096908d78c532c6ede83998443165
nivinjoseph <nivinjoseph@outlook.com>
2021-11-13 22:28:17 -0500

    preparing to publish new version

--- dist/hmr-helper.js
+++ dist/hmr-helper.js
@@ -11,11 +11,14 @@ class HmrHelper {
     static get devFs() { return this._devFs; }
     static get outputPath() { return this._outputPath; }
     static configure() {
+        const devFs = memfs_1.fs;
+        devFs.join = path.join.bind(path); // no need to bind
+        this._devFs = devFs;
         const config = require(path.join(process.cwd(), "webpack.config.js"));
         this._outputPath = config.output.path;
     }
 }
 exports.HmrHelper = HmrHelper;
-HmrHelper._devFs = memfs_1.fs;
+HmrHelper._devFs = null;
 HmrHelper._outputPath = null;
 //# sourceMappingURL=hmr-helper.js.map
