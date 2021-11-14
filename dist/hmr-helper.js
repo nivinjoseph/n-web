e4053b43249e59cf2810b17714a28dbffe8a7749
nivinjoseph <nivinjoseph@outlook.com>
2021-11-13 22:36:37 -0500

    preparing to publish new version

--- dist/hmr-helper.js
+++ dist/hmr-helper.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.HmrHelper = void 0;
 const memfs_1 = require("memfs");
 const path = require("path");
+const mkdirp = require("mkdirp");
 class HmrHelper {
     /**
      * @static
@@ -11,8 +12,9 @@ class HmrHelper {
     static get devFs() { return this._devFs; }
     static get outputPath() { return this._outputPath; }
     static configure() {
-        const devFs = memfs_1.fs;
-        devFs.join = path.join.bind(path); // no need to bind
+        const devFs = (0, memfs_1.createFsFromVolume)(new memfs_1.Volume());
+        devFs.join = path.join.bind(path);
+        devFs.mkdirp = mkdirp.bind(mkdirp);
         this._devFs = devFs;
         const config = require(path.join(process.cwd(), "webpack.config.js"));
         this._outputPath = config.output.path;
